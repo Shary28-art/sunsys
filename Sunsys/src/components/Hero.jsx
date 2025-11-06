6a33df1938dea42ecff8b4223aee6cda5500cf45
naveenkumar80 <naveenumar80032@gmail.com>
2025-11-06 15:51:32 +0530

    hero section buttons updated

--- Sunsys/src/components/Hero.jsx
+++ Sunsys/src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 export default function Hero() {
   return (
@@ -7,8 +9,8 @@ export default function Hero() {
         <h1>Power Your Life with Solar Energy</h1>
         <p>Clean. Sustainable. Affordable. Join the movement for a greener tomorrow with SunSys.</p>
         <div className="hero-btns">
-          <a href="about.html" className="btn">Learn More</a>
-          <a href="contacts.html" className="btn btn-secondary">Get a Quote</a>
+          <Link to = "/about" className="btn">Learn More</Link>
+          <Link to ="/contact" className="btn btn-secondary">Get a Quote</Link>
         </div>
       </div>
     </section>
